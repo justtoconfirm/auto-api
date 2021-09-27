88046fd9808ff52153aff593809e5d8124da993e
justtoconfirm <office@justtoconfirm.co.uk>
2021-09-27 20:42:47 +0100

    Setup initial endpoint for the first car

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const cors = require('cors');
 const app = express();
 const port = 8080;
 
-const foo = require('./data.json');
-
 // Use middleware to parse the data into JSON
 app.use(express.json());
 // Use middleware for CORS to allow endpoint to be hit from different locations
 app.use(cors());
 
 // Function to handle GET requests to create an endpoint
-app.get('/cars', (req, res) => {
-	res.status(200).send(foo);
+app.get('/cars/volkswagen/golf',(req, res) => {
+
+	res.status(200).send(require('./data/volkswagen/golf.json'));
+	// Sending a 404 when data is not found when hitting the endpoint
+	res.status(404).send('Data not found');
 });
 
 app.listen(port, () => {
